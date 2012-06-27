81efd0464a2cda3d84036c95121f92ed0e9fb3c8
PrplHaz4 <JJHayesIII@gmail.com>
2012-06-26 20:43:58 -0700

    Updated Foursquare provider for API changes - authorizeUrl and profileUrl http://stackoverflow.com/questions/10745881/canonicalurl-gone-from-foursquare-user-object

--- hybridauth/Hybrid/Providers/Foursquare.php
+++ hybridauth/Hybrid/Providers/Foursquare.php
@@ -21,7 +21,7 @@ class Hybrid_Providers_Foursquare extends Hybrid_Provider_Model_OAuth2
 
 		// Provider apis end-points
 		$this->api->api_base_url  = "https://api.foursquare.com/v2/";
-		$this->api->authorize_url = "https://foursquare.com/oauth2/authenticate";
+		$this->api->authorize_url = "https://foursquare.com/oauth2/authorize";
 		$this->api->token_url     = "https://foursquare.com/oauth2/access_token"; 
 
 		$this->api->sign_token_name = "oauth_token";
@@ -45,7 +45,7 @@ class Hybrid_Providers_Foursquare extends Hybrid_Provider_Model_OAuth2
 		$this->user->profile->lastName      = $data->lastName;
 		$this->user->profile->displayName   = trim( $this->user->profile->firstName . " " . $this->user->profile->lastName );
 		$this->user->profile->photoURL      = $data->photo;
-		$this->user->profile->profileURL    = $data->canonicalUrl;
+		$this->user->profile->profileURL    = "https://www.foursquare.com/user/" . $data->id;
 		$this->user->profile->gender        = $data->gender;
 		$this->user->profile->city          = $data->homeCity;
 		$this->user->profile->email         = $data->contact->email;
